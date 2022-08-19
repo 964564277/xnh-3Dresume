bba9717d1824a1f4541e2a246f70fa8ca4342fd8
惜年画 <964564277@qq.com>
2022-08-20 00:20:42 +0800

    打包

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     app: ['@babel/polyfill', './src/app.js']
   },
   output: {
-    path: path.resolve(__dirname,'./dist'),     
-		filename: 'bundle.js'
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
   },
   module: {
     rules: [
